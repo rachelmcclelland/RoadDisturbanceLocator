19bc87e958ec5f079362abf7973f844a0f159bcd
Rachel <g00337231@gmit.ie>
2019-02-15 12:01:53 +0000

    geolocation working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation/ngx'; 
+
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  options : GeolocationOptions;
+  currentPos : Geoposition;
+
+  constructor(public geolocation: Geolocation) {
+
+  }
+
+  ionViewDidEnter(){
+    this.getUserPosition();
+  }
+
+  getUserPosition(){
+    this.options = {
+        enableHighAccuracy : true
+    };
+
+    this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
+
+        this.currentPos = pos;      
+        console.log(pos);
+    //    console.log(pos.coords.latitude, pos.coords.longitude);
+
+    },(err : PositionError)=>{
+        console.log("error : " + err.message);
+    });
+}
 }
