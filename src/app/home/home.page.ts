90429187673b05ac4a0d95151aa373d960f010e8
Rachel <g00337231@gmit.ie>
2019-02-21 14:29:47 +0000

    server is fully working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation/ngx'; 
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { RequestOptions, RequestMethod } from '@angular/http';
+import {DataService} from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +11,10 @@ import { RequestOptions, RequestMethod } from '@angular/http';
 })
 export class HomePage {
 
-  @ViewChild("latitude") latitude;
-
-  @ViewChild("longitude") longitude;
-  public items : Array<any> = [];
-
   options : GeolocationOptions;
   currentPos : Geoposition;
 
-  constructor(public geolocation: Geolocation, public http: HttpClient) {
+  constructor(public geolocation: Geolocation, public http: HttpClient, private service:DataService) {
 
   }
 
@@ -36,48 +32,12 @@ export class HomePage {
         this.currentPos = pos;      
         console.log(pos);
     //    console.log(pos.coords.latitude, pos.coords.longitude);
-    this.getData();
-    this.sendData(pos.coords.latitude, pos.coords.longitude);
+        this.service.sendData(pos.coords.latitude, pos.coords.longitude);
+
+        this.service.getData();
 
     },(err : PositionError)=>{
         console.log("error : " + err.message);
     });
   }
-
-  getData(){
-    
-    this.http.get('http://localhost/getData.php').subscribe((data : any) =>
-    {
-       console.dir(data);
-       this.items = data;
-       
-    },
-    (error : any) =>
-    {
-       console.dir(error);
-    });
-
-    console.log(this.items);
-
-  }
-
-  private baseURI : string  = "http://localhost/";
-
-  sendData(lat, long){
-    let headers 	: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
-        options 	: any		= { "key" : "create", "latitude" : lat, "longitude" : long },
-        url       : any      	= this.baseURI + "sendData.php";
-
-    this.http.post(url, JSON.stringify(options), headers)
-    .subscribe((data : any) =>
-    {
-       // If the request was successful notify the user
-//this.sendNotification(`Congratulations the technology: ${name} was successfully added`);
-      console.log("lat and long was sent to the database");
-    },
-    (error : any) =>
-    {
-        console.log(error);
-    });
- }
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+    public items : Array<any> = [];
+    private baseURI : string  = "http://localhost/";
+
+    constructor(private http: HttpClient) { }
+
+    getData(){
+    
+        this.http.get('http://localhost/getData.php').subscribe((data : any) =>
+        {
+           console.dir(data);
+           this.items = data;
+           
+        },
+        (error : any) =>
+        {
+           console.dir(error);
+        });
+    
+        //console.log(this.items);
+    }
+
+    sendData(lat, long){
+        let headers 	: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+            options 	: any		= { "key" : "create", "latitude" : lat, "longitude" : long },
+            url       : any      	= this.baseURI + "sendData.php";
+    
+        this.http.post(url, JSON.stringify(options), headers)
+        .subscribe((data : any) =>
+        {
+           // If the request was successful notify the user
+    //this.sendNotification(`Congratulations the technology: ${name} was successfully added`);
+          console.log("lat and long was sent to the database");
+        },
+        (error : any) =>
+        {
+            console.log(error);
+        });
+     }
+}
